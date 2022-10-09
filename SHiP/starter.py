8b8f715a748e3ad5821bf8c02e86ebebf2ba3b4f
Art-1313 <gromov.ak@phystech.edu>
2022-10-09 22:17:27 +0300

    new cm

--- SHiP/starter.py
+++ SHiP/starter.py
@@ -20,6 +20,8 @@ from sklearn.metrics import roc_curve, roc_auc_score
 from sklearn.cluster import KMeans
 from sklearn.model_selection import GridSearchCV
 
+from sklearn.preprocessing import StandardScaler
+
 from catboost import CatBoostClassifier
 
 from imblearn.over_sampling import SMOTE
@@ -47,6 +49,19 @@ data['type'] = np.hstack((np.ones_like(w_bkg), np.zeros_like(w_sig)))
 
 ############
 
+def print_err(a, a_err, vis=True):
+    a = np.array([a]).flatten()
+    a_err = np.array([a_err]).flatten()
+    val = np.zeros_like(a)
+    sig = np.zeros_like(a_err)
+    for i in range(a.size):
+        val[i] = np.round(a[i], decimals = round(1 * (np.trunc(np.log10(a_err[i]) < 0)) - np.trunc(np.log10(a_err[i]))))
+        sig[i] = np.round(a_err[i], decimals = round(1 * (np.trunc(np.log10(a_err[i]) < 0)) - np.trunc(np.log10(a_err[i]))))
+        if vis: print(val[i], '\pm', sig[i])
+    return val, sig
+
+############
+
 def get_metrics(X_train, X_test, y_train, y_test, cat_ft=[], vis=True, auc=False, proba=False):
 
     log_reg_auc = []
@@ -59,11 +74,21 @@ def get_metrics(X_train, X_test, y_train, y_test, cat_ft=[], vis=True, auc=False
     cat_bst_proba = []
     xg_bst_proba = []
 
+    scale = StandardScaler()
+
+    cols = list(X_train[0].columns.values)
+
+    for x in cat_ft: cols.remove(x)
+
     if vis:
         plt.figure(figsize=(25, 25))
-        plt.suptitle('ROC curves for different classifiers', y=0.92)
 
     for i in range(len(states)):
+
+        scale.fit(X_train[i][cols])
+
+        X_train[i][cols] = scale.transform(X_train[i][cols])
+        X_test[i][cols] = scale.transform(X_test[i][cols])
 
         log_reg = LogisticRegression(n_jobs=-1, random_state=42, max_iter=10 ** 6).fit(X_train[i], y_train[i]).predict_proba(X_test[i])
         log_reg_auc.append(roc_auc_score(y_test[i], log_reg[:, 1]))
@@ -74,7 +99,7 @@ def get_metrics(X_train, X_test, y_train, y_test, cat_ft=[], vis=True, auc=False
             fpr, tpr, _ = roc_curve(y_test[i], log_reg[:, 1])
             plt.plot(fpr, tpr)
         
-        cat_bst = CatBoostClassifier(verbose=False, random_state=42, cat_features=cat_ft).fit(X_train[i], y_train[i]).predict_proba(X_test[i])
+        cat_bst = CatBoostClassifier(verbose=False, random_state=42, cat_features=cat_ft, n_estimators=2000).fit(X_train[i], y_train[i]).predict_proba(X_test[i])
         cat_bst_auc.append(roc_auc_score(y_test[i], cat_bst[:, 1]))
         cat_bst_proba.append(cat_bst[:, 1])
 
@@ -83,7 +108,7 @@ def get_metrics(X_train, X_test, y_train, y_test, cat_ft=[], vis=True, auc=False
             fpr, tpr, _ = roc_curve(y_test[i], cat_bst[:, 1])
             plt.plot(fpr, tpr)
 
-        xg_bst = XGBClassifier().fit(X_train[i], y_train[i]).predict_proba(X_test[i])
+        xg_bst = XGBClassifier(n_jobs=-1).fit(X_train[i], y_train[i]).predict_proba(X_test[i])
         xg_bst_auc.append(roc_auc_score(y_test[i], xg_bst[:, 1]))
         xg_bst_proba.append(xg_bst[:, 1])
 
@@ -92,45 +117,49 @@ def get_metrics(X_train, X_test, y_train, y_test, cat_ft=[], vis=True, auc=False
             fpr, tpr, _ = roc_curve(y_test[i], xg_bst[:, 1])
             plt.plot(fpr, tpr)
 
-        rnd_frst = RandomForestClassifier(n_jobs=-1, random_state=42).fit(X_train[i], y_train[i]).predict_proba(X_test[i])
+        rnd_frst = RandomForestClassifier(n_jobs=-1, random_state=42, n_estimators=2000, min_samples_leaf=5).fit(X_train[i], y_train[i]).predict_proba(X_test[i])
         rnd_frst_auc.append(roc_auc_score(y_test[i], rnd_frst[:, 1]))
         rnd_frst_proba.append(rnd_frst[:, 1])
 
         if vis:
             plt.subplot(2, 2, 2)
-            fpr, tpr, _ = roc_curve(y_test[i], rnd_frst[:, 1])
+            fpr, tpr, _ = roc_curve(y_test[i], rnd_frst[:, 1], drop_intermediate=False)
             plt.plot(fpr, tpr, label='state {}'.format(states[i]))
 
     if vis:
         box = {'facecolor':'black', 'edgecolor': 'red', 'boxstyle': 'round'}
 
         plt.subplot(2, 2, 1)
-        plt.title('Logistic Regression')
+        plt.title('Logistic Regression', fontdict={'size': 50, 'fontname': 'Times New Roman'})
         plt.plot([0, 1], [0, 1], linestyle='--', color='black')
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.text(0.8, 0.05, 'AUC = {:4f} ± {:4f}'.format(np.mean(log_reg_auc), np.std(log_reg_auc)), horizontalalignment = 'center', bbox = box, color = 'white')
+        plt.xlabel('Fake rate')
+        plt.ylabel('Efficiency')
+        val, err = print_err(np.mean(log_reg_auc), np.std(log_reg_auc), vis=False)
+        plt.text(0.7, 0.05, 'AUC = {} ± {}'.format(val[0], err[0]), fontdict={'size': 40, 'fontname': 'Times New Roman'}, horizontalalignment = 'center', bbox = box, color = 'white')
 
         plt.subplot(2, 2, 3)
-        plt.title('CatBoost')
+        plt.title('CatBoost', fontdict={'size': 50, 'fontname': 'Times New Roman'})
         plt.plot([0, 1], [0, 1], linestyle='--', color='black')
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.text(0.8, 0.05, 'AUC = {:4f} ± {:4f}'.format(np.mean(cat_bst_auc), np.std(log_reg_auc)), horizontalalignment = 'center', bbox = box, color = 'white')
+        plt.xlabel('Fake rate')
+        plt.ylabel('Efficiency')
+        val, err = print_err(np.mean(cat_bst_auc), np.std(cat_bst_auc), vis=False)
+        plt.text(0.7, 0.05, 'AUC = {} ± {}'.format(val[0], err[0]), fontdict={'size': 40, 'fontname': 'Times New Roman'}, horizontalalignment = 'center', bbox = box, color = 'white')
 
         plt.subplot(2, 2, 4)
-        plt.title('XGBoost')
+        plt.title('XGBoost', fontdict={'size': 50, 'fontname': 'Times New Roman'})
         plt.plot([0, 1], [0, 1], linestyle='--', color='black')
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.text(0.8, 0.05, 'AUC = {:4f} ± {:4f}'.format(np.mean(xg_bst_auc), np.std(rnd_frst_auc)), horizontalalignment = 'center', bbox = box, color = 'white')
+        plt.xlabel('Fake rate')
+        plt.ylabel('Efficiency')
+        val, err = print_err(np.mean(xg_bst_auc), np.std(xg_bst_auc), vis=False)
+        plt.text(0.7, 0.05, 'AUC = {} ± {}'.format(val[0], err[0]), fontdict={'size': 40, 'fontname': 'Times New Roman'}, horizontalalignment = 'center', bbox = box, color = 'white')
 
         plt.subplot(2, 2, 2)
-        plt.title('Randomn Forest')
+        plt.title('Random Forest', fontdict={'size': 50, 'fontname': 'Times New Roman'})
         plt.plot([0, 1], [0, 1], linestyle='--', color='black')
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.text(0.8, 0.05, 'AUC = {:4f} ± {:4f}'.format(np.mean(rnd_frst_auc), np.std(rnd_frst_auc)), horizontalalignment = 'center', bbox = box, color = 'white')
+        plt.xlabel('Fake rate')
+        plt.ylabel('Efficiency')
+        val, err = print_err(np.mean(rnd_frst_auc), np.std(rnd_frst_auc), vis=False)
+        plt.text(0.7, 0.05, 'AUC = {} ± {}'.format(val[0], err[0]), fontdict={'size': 40, 'fontname': 'Times New Roman'}, horizontalalignment = 'center', bbox = box, color = 'white')
 
         plt.legend(loc = 'upper right')
 
